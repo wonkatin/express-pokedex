af829c26ea9a2c2b7ca535559a151c7cfd04e9cb
h64 <hhongnr@gmail.com>
2021-02-25 13:04:29 -0800

    refactor for clarity

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -2,11 +2,7 @@ const router = require('express').Router()
 const axios = require('axios')
 
 router.get('/', async (req, res) => {
-    const pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151'
-    const response = await axios.get(pokemonUrl)
-    const pokemon = response.data.results
-    
-    res.render('pokemon/index', { pokemon: pokemon })
+    res.render('pokemon/index')
 })
 
 module.exports = router
